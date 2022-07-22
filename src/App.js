3c414cc2d2992df61f4af40cf3eeee3d87b0e3e4
Walid-Belkassmi <belkassmi.walid@gmail.com>
2022-07-22 11:44:37 +0200

    addTask()

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from "react";
+import Form from "./components/Form";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      task: [],
+    };
+  }
+
+  addTask = (string) => {
+    const clonedAddTask = [...this.state.task, string.target.value];
+
+    this.setState({
+      task: clonedAddTask,
+    });
+  };
+
   render() {
-    return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+    return (
+      <div>
+        <Form />
+      </div>
+    );
   }
 }
 
---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Form extends React.Component {
+  render() {
+    return (
+      <div className="w-full max-w-xs">
+        <p>test</p>
+      </div>
+    );
+  }
+}
+
+export default Form;
