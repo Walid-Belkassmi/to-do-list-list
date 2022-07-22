b8997e44b01b21387a3ff88ebb6b1cfa54ac05b1
Caroline <alyseflorez@hotmail.fr>
2022-07-22 11:39:55 +0200

    form css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Form from "./components/Form";
 
 class App extends React.Component {
   render() {
-    return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+    return (
+      <><h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <Form/>
+    </>
+    )
   }
 }
 
